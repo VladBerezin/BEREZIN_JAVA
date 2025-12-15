cbba468381bd83d1abbbe0954dce4d5d23590921
VladBerezin <berezinvlad565@gmail.com>
2025-12-16 00:09:38 +0300

    Add files via upload

---
+++ pr11/src/Hero.java
@@ -0,0 +1,18 @@
+public class Hero {
+    String name;
+    int energy;
+
+    public Hero(String name, int energy) {
+        this.name = name;
+        this.energy = energy;
+    }
+
+    public int getEnergy()
+    {
+        return energy;
+    }
+
+    public String toString() {
+        return name + ", энергия: " + energy;
+    }
+}
